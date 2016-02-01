6af9d93df81654a97c41e8526c39d3fef8bade97
lokenath007 <lokenathghosh@gmail.com>
2016-02-01 14:08:57 -0600

    I am lokenath

---
+++ src/test/java/com/lexmark/mavnetest/testfirsttest.java
@@ -0,0 +1,18 @@
+package com.lexmark.mavnetest;
+
+import org.testng.annotations.Test;
+
+public class testfirsttest {
+
+@Test
+	public void test1()
+	{
+		System.out.println(" I am in maven1");
+	}
+
+@Test
+public void test2()
+{
+	System.out.println(" I am in maven2");
+}
+}
